871f21cb5bbeb540e4c61c82da10ba9c851fcd6a
aamckennapro <am286517@ohio.edu>
2019-01-22 18:38:42 -0500

    This program says hello to Git.

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aaron McKenna (aamckennapro)
+ *        Date: Tuesday, January 22nd, 2019
+ * Description: Says hello to Git.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
